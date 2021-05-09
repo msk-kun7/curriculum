27d394c69adda0f0d1127e328863c56f651beada
“m-taoka” <m-taoka@y-i-group.co.jp>
2021-05-09 11:03:37 +0900

    2章 チェックテスト
    課題提出テスト

--- JavaStady/Check2/src/check/Check.java
+++ JavaStady/Check2/src/check/Check.java
@@ -1,22 +1,27 @@
 package check;
-
 import constants.Constants;
 
-public class Check extends Constants {
+public class Check  {
+	
+	private String firstName="田岡";
+	private String lastName="真輝";
+	
+	private void printName(String firstName,String lastName){
+	System.out.println("printlnメソッド⇨"+this.firstName+this.lastName);
+	}
 	
 	public static void main(String[] args) { 
 		
 		//String型の変数firstNameに"姓"を代入
 		//String型の変数lastNameに"名"を代入　
 		//getNameメソッド(戻り値)
-		String firstName="田岡";
-    	String lastName="真輝";
-    	System.out.println(getName(firstName, lastName));
+		Check check = new Check();
+		check.printName(check.firstName,check.lastName);
     	
-    	Pet pet = new Pet(CHECK_CLASS_JAVA, CHECK_CLASS_HOGE);
+    	Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 	    pet.introduce();
 	    
-	    RobotPet robotpet = new RobotPet(CHECK_CLASS_R2D2,CHECK_CLASS_LUKE);
+	    RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
 	    robotpet.introduce();
 	    
 	}
