e2bd9a39418f4a627bea3b14baac2861885166ff
“m-taoka” <m-taoka@y-i-group.co.jp>
2021-05-10 05:51:53 +0900

    2章 チェックテスト
    課題提出テスト

--- JavaStady/Check2/src/check/Check.java
+++ JavaStady/Check2/src/check/Check.java
@@ -7,14 +7,13 @@ public class Check  {
 	private String lastName="真輝";
 	
 	private void printName(String firstName,String lastName){
+		
 	System.out.println("printlnメソッド⇨"+this.firstName+this.lastName);
+	
 	}
 	
 	public static void main(String[] args) { 
 		
-		//String型の変数firstNameに"姓"を代入
-		//String型の変数lastNameに"名"を代入　
-		//getNameメソッド(戻り値)
 		Check check = new Check();
 		check.printName(check.firstName,check.lastName);
     	
@@ -25,8 +24,5 @@ public class Check  {
 	    robotpet.introduce();
 	    
 	}
-	//return文：public static 戻り値の型　メソッド名(引数リスト){return 戻り値}
-	 public static String getName(String firstName, String lastName) {
-	    	return firstName + lastName;
-	    }
+
 }
